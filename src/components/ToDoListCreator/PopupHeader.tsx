0f5ee35c08a536b7d92a7cf3adc64452afb1668d
Sebastian Mastalerz <97692547+Jacket235@users.noreply.github.com>
2025-10-01 20:22:48 +0200

    Small changes

--- src/components/ToDoListCreator/PopupHeader.tsx
+++ src/components/ToDoListCreator/PopupHeader.tsx
@@ -1,10 +1,18 @@
 import './popupHeader.css';
+import { useContext } from 'react';
+import OverlayContext from '../../Context/OverlayContext';
 
 export function PopupHeader() {
+    const overlay = useContext(OverlayContext);
+
+    const handleClosePopup = () => {
+        overlay?.closeOverlay();
+    }
+
     return(
         <div className='popup-header'>
             Create a new todo list
-            <button>
+            <button onClick={handleClosePopup}>
                 <img src='exit.svg' width="16px" height="16px" />
             </button>
         </div>
