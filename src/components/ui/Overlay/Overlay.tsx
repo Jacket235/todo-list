353db1b78fcd05c11c81a45501c8c16449b8e3de
Sebastian Mastalerz <97692547+Jacket235@users.noreply.github.com>
2025-10-01 20:13:58 +0200

    Bug fix
    Overlay doesnt close when interacting with content now

--- src/components/ui/Overlay/Overlay.tsx
+++ src/components/ui/Overlay/Overlay.tsx
@@ -4,7 +4,7 @@ import { OverlayProps } from '../../../Types';
 export function Overlay({ onClose, children }: OverlayProps) {
     return (
         <div className="overlay" onClick={onClose}>
-            <div className='overlay-content'>
+            <div className='overlay-content' onClick={e => e.stopPropagation()}>
                 {children}
             </div>
         </div>
