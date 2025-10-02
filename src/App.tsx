cebf97acbdef75232a615585e7453495a64cc865
Sebastian Mastalerz <97692547+Jacket235@users.noreply.github.com>
2025-10-02 19:13:16 +0200

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,31 @@ import { TodoListType } from './Types';
 function App() {
     const [todos, setTodos] = useState<TodoListType[]>([]);
 
-    const todos1 = [{
-        id: crypto.randomUUID(),
-        title: "Personal Tasks",
-        tasks: {
-            complete: [
-                { id: crypto.randomUUID(), text: "Buy groceries: milk, bread, eggs, and coffee"},
-                { id: crypto.randomUUID(), text: "Morning jog – 5km around the park"}
-            ],
-            inprogress: [
-                { id: crypto.randomUUID(), text: "Finish React project and push to GitHub"}
-                
-            ],
-            incomplete: [
-                { id: crypto.randomUUID(), text: "Read 20 pages of 'Clean Code'"},
-                { id: crypto.randomUUID(), text: "Call mom in the evening"}
-            ]
-        }},
-    ];
+    useEffect(() => {
+        setTodos([{
+            id: crypto.randomUUID(),
+            title: "Personal Tasks",
+            tasks: {
+                complete: [
+                    { id: crypto.randomUUID(), text: "Buy groceries: milk, bread, eggs, and coffee"},
+                    { id: crypto.randomUUID(), text: "Morning jog – 5km around the park"}
+                ],
+                inprogress: [
+                    { id: crypto.randomUUID(), text: "Finish React project and push to GitHub"}
+                    
+                ],
+                incomplete: [
+                    { id: crypto.randomUUID(), text: "Read 20 pages of 'Clean Code'"},
+                    { id: crypto.randomUUID(), text: "Call mom in the evening"}
+                ]
+            }}]
+        );
+    }, [])
 
     return (
         <div className="App">
             <Header />
-            <Main todos={todos1} />
+            <Main todos={todos} />
         </div>
     );
 }
--- src/components/ToDoListCreator/ToDoListCreatorContent.tsx
+++ src/components/ToDoListCreator/ToDoListCreatorContent.tsx
@@ -1,10 +1,7 @@
-import NewToDoListContext from '../../Context/NewToDoListContext';
-import { Task } from '../../Types';
 import { TitleInput } from './TitleInput'
 import { ToDoListAction } from './ToDoListAction';
 import './toDoListCreatorContent.css'
 import { ToDoListTasks } from './ToDoListTasks'
-import { useState } from 'react';
 
 export function ToDoListCreatorContent() {
     return(
