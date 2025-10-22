54a4efab8576c355d8f264c96afbb0e50619696d
Sebastian Mastalerz <97692547+Jacket235@users.noreply.github.com>
2025-10-22 15:49:41 +0200

    Small tweaks

--- src/components/ToDoListEdit/ToDoListEditor.tsx
+++ src/components/ToDoListEdit/ToDoListEditor.tsx
@@ -1,11 +1,11 @@
-import './toDoListEdit.css'
+import './toDoListEditor.css'
 import TodoListEditorContext from '../../context/TodoListEditorContext';
 
 export function ToDoListEdit() {
     return (
         <TodoListEditorContext.Provider value={{}}>
             <div className="todo-list-edit">
-                Edit todo lists.
+                Create todo lists.
             </div>
         </TodoListEditorContext.Provider>
     );
---
+++ src/components/ToDoListEdit/ToDoListEditorHeader.tsx
@@ -0,0 +1,10 @@
+import { useOverlayContext } from '../../hooks/useOverlayContext';
+import { ToDoListHeader } from '../shared/ToDoListHeader';
+
+export function ToDoListEditorHeader() {
+    const { closeOverlay } = useOverlayContext();
+
+    return(
+        <ToDoListHeader title='Edit the current list' icon='exit.svg' iconAlt='exit' onButtonClick={ closeOverlay } />
+    );
+}
