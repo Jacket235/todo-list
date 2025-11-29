7751162763e3fa7dc2ba6708e47e96fc86622dff
Sebastian Mastalerz <97692547+Jacket235@users.noreply.github.com>
2025-11-29 12:56:31 +0100

    Delete list button functionality

--- src/components/ToDoListEdit/components/ToDoListAction.tsx
+++ src/components/ToDoListEdit/components/ToDoListAction.tsx
@@ -1,13 +1,35 @@
+import { useOverlayContext } from '../../../hooks/useOverlayContext';
+import { useTodoListEditorContext } from '../../../hooks/useTodoListEditorContext';
+import { useTodosContext } from '../../../hooks/useTodosContext';
 import '../styles/to-do-list-action.css';
 
 export function ToDoListAction() {
+    const { id, title, tasks } = useTodoListEditorContext();
+    const { setTodos } = useTodosContext();
+    const { closeOverlay } = useOverlayContext();
+
+    const handleDeleteList = () => {
+        const updated = setTodos(prev => {
+            const updated = prev.filter(todo => todo.id !== id)
+            localStorage.setItem("todos", JSON.stringify(updated));
+
+            return updated
+        });
+        
+        closeOverlay();
+    }
+
+    const handleClose = () => {
+        closeOverlay();
+    }
+
     return (
         <div className="editor-list-action">
             <div className="delete-list-button-container">
-                <button>Delete list</button>
+                <button onClick={handleDeleteList}>Delete list</button>
             </div>
             <div className="default-button-container">
-                <button>Cancel</button>
+                <button onClick={handleClose}>Cancel</button>
                 <button>Save</button>
             </div>
         </div>
